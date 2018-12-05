0d0b09843af0e534611fb339b9f25797991f009d
eliaxh <f.duersch@gmx.de>
2018-12-05 22:27:55 +0100

    basic functionality

---
+++ src/storage/repository/BaseRepository.ts
@@ -0,0 +1,21 @@
+import { ReadInterface } from "../interfaces/ReadInterface";
+import { WriteInterface } from "../interfaces/WriteInterface";
+
+export abstract class BaseRepository<T> implements ReadInterface<T>, WriteInterface<T>{
+   
+    create(item: T): Promise<boolean> {
+        throw new Error("Method not implemented.");
+    }
+    update(id: string, item: T): Promise<boolean> {
+        throw new Error("Method not implemented.");
+    }
+    delete(id: string): Promise<boolean> {
+        throw new Error("Method not implemented.");
+    }
+    find(item: T): Promise<T[]> {
+        throw new Error("Method not implemented.");
+    }
+    findOne(id: string): Promise<T> {
+        throw new Error("Method not implemented.");
+    }
+}
